4c2dd8a6b5c216f1212e5675d59c7a831fe71aeb
Ahmed ElZubair <ae87@hw.ac.uk>
2023-06-17 00:18:36 +0400

    checkpoint 1 header template done

--- hyke/src/App.js
+++ hyke/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="text-gray-600 text-4xl">
-      <h1>Hyke</h1>
+      <Header/>
     </div>
   );
 }
