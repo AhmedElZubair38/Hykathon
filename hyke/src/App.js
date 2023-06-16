40029b0c8af1cba26f523a64d3f41c725c0b9755
Ahmed ElZubair <ae87@hw.ac.uk>
2023-06-17 01:16:17 +0400

    home page not bad template

--- hyke/src/App.js
+++ hyke/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Home from './modules/Home';
 
 function App() {
   return (
-    <div className="text-gray-600 text-4xl">
+    <div>
+      
       <Header/>
+      <Home/>
+
     </div>
   );
 }
---
+++ hyke/src/modules/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../../components/Hero'
+import Products from '../../components/Products'
+
+function Home() {
+  return (
+    <>
+        <Hero/>
+        <Products/>
+    </>
+  )
+}
+
+export default Home
