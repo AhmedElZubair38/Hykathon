a738158e6b21a1febf9039a0b67db34dc706385e
Ahmed ElZubair <ae87@hw.ac.uk>
2023-06-17 13:45:50 +0400

    man

--- hyke/src/modules/Home/index.js
+++ hyke/src/modules/Home/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Hero from '../../components/Hero'
 import Products from '../../components/Products'
+import Features from '../../components/Features'
 
 function Home() {
   return (
     <>
         <Hero/>
+        <h2 className="text-center text-5xl mt-12">Products</h2>
         <Products/>
+        <Features/>
     </>
   )
 }
--- hyke/tailwind.config.js
+++ hyke/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'black': '#1D1D1D',
+      'blue': '#2272FF',
+    },
   },
   plugins: [],
 }
